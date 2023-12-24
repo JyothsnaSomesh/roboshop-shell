c226de9cdc405791bc96c6460b6ebe97270d08ef
JyothsnaSomesh <joesomesh@gmail.com>
2023-12-23 22:23:31 -0600

    roboshop-shell

--- roboshop.sh
+++ roboshop.sh
@@ -10,10 +10,10 @@ do
     if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
         INSTANCE_TYPE="t3.small"
-        else 
+    else 
         INSTANCE_TYPE="t2.micro"
-fi
+    fi
 
-aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids 
-sg-042b3bca4427fb7b3 --tag-specifications "ResourceType=instance, Tags=[{key=Name,Value=$i}]" 
+     aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids 
+     sg-042b3bca4427fb7b3 --tag-specifications "ResourceType=instance, Tags=[{key=Name,Value=$i}]" 
 done
