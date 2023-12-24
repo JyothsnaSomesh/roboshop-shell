7d9bd670bdce379f360a7cfae88392940ec78408
JyothsnaSomesh <joesomesh@gmail.com>
2023-12-23 22:57:41 -0600

    roboshop-shell

--- roboshop.sh
+++ roboshop.sh
@@ -14,6 +14,6 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-     aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-042b3bca4427fb7b3 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+     aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-042b3bca4427fb7b3 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text
       
 done
