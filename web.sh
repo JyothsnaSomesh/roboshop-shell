167c109948a660a17f0e96c85cdf79ba1d46b7a7
JyothsnaSomesh <joesomesh@gmail.com>
2023-12-24 14:18:16 -0600

    roboshop-shell

--- web.sh
+++ web.sh
@@ -35,6 +35,14 @@ dnf install nginx -y &>>$LOGFILE
 
 VALIDATE $? "installing nginx"
 
+systemctl enable nginx
+
+VALIDATE $? "enabled nginx"
+
+systemctl start nginx
+
+VALIDATE $? "started nginx"
+
 rm -rf /usr/share/nginx/html/* &>>$LOGFILE
 
 VALIDATE $? "removing default website"
