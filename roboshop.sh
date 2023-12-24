af91f287d417293c1cf52522a5a34748e35e0ad0
JyothsnaSomesh <joesomesh@gmail.com>
2023-12-23 23:47:53 -0600

    roboshop-shell

--- roboshop.sh
+++ roboshop.sh
@@ -3,6 +3,8 @@
 AMI=ami-03265a0778a880afb
 SG_ID=sg-042b3bca4427fb7b3
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "User" "cart" "shipping" "Payment" "web" "ratings")
+Zone_ID=Z025729721K26XR9HFCPR
+DOMAIN_NAME="daws76devops.online"
 
 for i in "${INSTANCES[@]}"
 do  
@@ -13,7 +15,25 @@ do
     else 
         INSTANCE_TYPE="t2.micro"
     fi
-
-     IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-042b3bca4427fb7b3 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
-     echo "$i: $IP_ADDRESS" 
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-042b3bca4427fb7b3 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    echo "$i: $IP_ADDRESS" 
+     
+    aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '
+  {
+    "Comment": "Testing creating a record set"
+    ,"Changes": [{
+      "Action"              : "CREATE"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$i.$DOMAIN_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$IP_ADDRESS'"
+        }]
+      }
+    }]
+  }
+  '
 done
